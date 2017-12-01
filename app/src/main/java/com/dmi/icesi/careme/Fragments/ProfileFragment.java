c1272f511e34500b6b493ef42865b8fe90b99a9f
Paula García <paulaagarcia1@gmail.com>
2017-12-01 02:56:45 -0500

    Email updating + profile photo...

--- app/src/main/java/com/dmi/icesi/careme/Fragments/ProfileFragment.java
+++ app/src/main/java/com/dmi/icesi/careme/Fragments/ProfileFragment.java
@@ -1,20 +1,52 @@
 package com.dmi.icesi.careme.Fragments;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ViewSwitcher;
 
 import com.dmi.icesi.careme.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.nostra13.universalimageloader.cache.memory.impl.WeakMemoryCache;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.ImageScaleType;
+import com.nostra13.universalimageloader.core.display.FadeInBitmapDisplayer;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+import static android.app.Activity.RESULT_OK;
+import static android.content.ContentValues.TAG;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -34,11 +66,31 @@ public class ProfileFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    private TextView username, email;
+    private TextView username, email, password;
+    private EditText et_email, et_pass;
+    private Button btnEmail, btnPass;
+    private ViewSwitcher vs;
+    private ViewSwitcher vsPass;
+    private FirebaseUser usuarioActual;
 
     private OnFragmentInteractionListener mListener;
     private Button signOutBut;
     private FirebaseAuth mAuth;
+
+    private TextView mensajeTextView;
+    private EditText mensajeEditText;
+
+    private StorageReference mStorageRef;
+
+    private CircleImageView holder;
+    private Bitmap pic;
+    private Uri uri;
+
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference ref = database.getReference();
+
+    private static final int SELECT_SINGLE_PICTURE = 101;
+    public static final String IMAGE_TYPE = "image/*";
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -78,10 +130,47 @@ public class ProfileFragment extends Fragment {
 
         username = (TextView) view.findViewById(R.id.tv_name);
         email = (TextView) view.findViewById(R.id.tv_email);
+        password = (TextView) view.findViewById(R.id.tv_password);
 
-        FirebaseUser usuarioActual = FirebaseAuth.getInstance().getCurrentUser();
+        et_email = (EditText) view.findViewById(R.id.et_email);
+        et_email = (EditText) view.findViewById(R.id.et_pass);
+
+        btnEmail = (Button) view.findViewById(R.id.btn_email);
+        btnPass = (Button) view.findViewById(R.id.btn_pass);
+
+        vs = (ViewSwitcher) view.findViewById(R.id.my_switcher);
+        vsPass = (ViewSwitcher) view.findViewById(R.id.switcherPass);
+
+        email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                vs.showNext();
+            }
+        });
+
+        btnEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                vs.showNext();
+                updateEmail(et_email.getText().toString());
+            }
+        });
+
+        btnPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                vsPass.showNext();
+                updatePassword(et_pass.getText().toString());
+            }
+        });
+
+
+
+        usuarioActual = FirebaseAuth.getInstance().getCurrentUser();
         username.setText(usuarioActual.getDisplayName());
         email.setText(usuarioActual.getEmail());
+        et_email.setText(usuarioActual.getEmail());
+       // password.setText(usuarioActual.getPassword());
 
         mAuth = FirebaseAuth.getInstance();
 
@@ -94,11 +183,84 @@ public class ProfileFragment extends Fragment {
             }
         });
 
+
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+
+        ImageLoader.getInstance().init(getConfig());
+
+        holder = (CircleImageView) view.findViewById(R.id.perfil);
+
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                for (DataSnapshot fotos : dataSnapshot.getChildren()) {
+                    if(fotos.getKey().contains("Ossa")) {
+                        //Si es un usuario especifico se puede hacer con el correo o
+                        //con el Uid con mAuth.getCurrentUser().getUid()
+                        Uri downloaded = Uri.parse(fotos.getValue().toString());
+                        Toast.makeText(getActivity().getApplicationContext(),"La foto es: " + downloaded.toString(),Toast.LENGTH_LONG).show();
+                        ImageLoader.getInstance().displayImage(downloaded.toString(),holder);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+
+            }
+        });
+
+        holder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType(IMAGE_TYPE);
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,
+                        "Seleccione una imagen"), SELECT_SINGLE_PICTURE);
+            }
+        });
+
         // Inflate the layout for this fragment
         return view;
     }
 
+    public void updateEmail(String email){
+        usuarioActual.updateEmail(email)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "User email address updated.");
+                        } else{
+                            Log.d(TAG, "Buuu NO address updated.");
+                        }
+                    }
+                });
+    }
 
+    public void updatePassword(String newPassword){
+
+        usuarioActual.updatePassword(newPassword)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "User password updated.");
+                        }
+                    }
+                });
+    }
+
+
+/*    public void TextViewClicked(View v) {
+        ViewSwitcher switcher = (ViewSwitcher) getView().findViewById(R.id.my_switcher);
+        switcher.showNext(); //or switcher.showPrevious();
+        TextView emailTV = (TextView) switcher.findViewById(R.id.tv_email);
+        //myTV.setText("value");
+    }*/
 
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
@@ -138,4 +300,65 @@ public class ProfileFragment extends Fragment {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == SELECT_SINGLE_PICTURE) {
+
+                Uri selectedImageUri = data.getData();
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), selectedImageUri);
+                    pic = bitmap;
+                    holder.setImageBitmap(bitmap);
+
+                    StorageReference storageReference = mStorageRef.child("Ossa");
+                    byte[] profileBytes = getBytesFromBitmap(pic,100);
+
+                    UploadTask uploadTask = null;
+                    uploadTask = storageReference.putBytes(profileBytes);
+
+                    uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Uri getDownloadUrl = taskSnapshot.getDownloadUrl();
+                            ref.child("Ossa").setValue(getDownloadUrl.toString());
+                            //Toast.makeText(getContext(), "Se subio foto de perfil a: " + getDownloadUrl.toString(), Toast.LENGTH_LONG).show();
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(getActivity().getApplicationContext(), "No se pudo cambiar la foto de perfil", Toast.LENGTH_LONG).show();
+                        }
+                    });
+
+                } catch (IOException e) {
+                    System.out.println("Error al cargar imagen");
+                }
+            }
+        } else {
+            Toast.makeText(getActivity(), "No se cargo ninguna imagen", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public static byte[] getBytesFromBitmap(Bitmap bitmap, int quality){
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, quality, stream);
+        return stream.toByteArray();
+    }
+
+    public ImageLoaderConfiguration getConfig(){
+        DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder()
+                .cacheOnDisk(true).cacheInMemory(true)
+                .cacheOnDisk(true).resetViewBeforeLoading(true)
+                .imageScaleType(ImageScaleType.EXACTLY)
+                .displayer(new FadeInBitmapDisplayer(300)).build();
+
+        ImageLoaderConfiguration configuration = new ImageLoaderConfiguration.Builder(getActivity().getApplicationContext())
+                .defaultDisplayImageOptions(defaultOptions)
+                .memoryCache(new WeakMemoryCache())
+                .diskCacheSize(100 * 1024 * 1024).build();
+
+        return configuration;
+    }
+
 }
--- app/src/main/java/com/dmi/icesi/careme/LoginActivity.java
+++ app/src/main/java/com/dmi/icesi/careme/LoginActivity.java
@@ -1,30 +1,14 @@
 package com.dmi.icesi.careme;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
-
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.AsyncTask;
-
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
@@ -34,11 +18,6 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static android.Manifest.permission.READ_CONTACTS;
 
 /**
  * A login screen that offers login via email/password.
@@ -128,6 +107,7 @@ public class LoginActivity extends AppCompatActivity {
 
         return valid;
     }
+
 
 
 }
